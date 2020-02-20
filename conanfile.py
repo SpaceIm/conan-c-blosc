83f11e164ce3939ebf41404454647227779adbc0
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-02-20 23:13:33 +0100

    pthread added to system_libs instead of libs

--- conanfile.py
+++ conanfile.py
@@ -105,4 +105,4 @@ class CbloscConan(ConanFile):
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
         if self.settings.os == "Linux":
-            self.cpp_info.libs.append("pthread")
+            self.cpp_info.system_libs.append("pthread")
