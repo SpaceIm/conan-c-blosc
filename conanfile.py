45ca3b12943a255287ad15d69f26405e8bd9f9f1
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-05-11 18:24:13 +0200

    more robust loop over patches

--- conanfile.py
+++ conanfile.py
@@ -66,7 +66,7 @@ class CbloscConan(ConanFile):
         os.rename(self.name + "-" + self.version, self._source_subfolder)
 
     def build(self):
-        for patch in self.conan_data["patches"][self.version]:
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
             tools.patch(**patch)
         cmake = self._configure_cmake()
         cmake.build()
